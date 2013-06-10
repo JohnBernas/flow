b9e583642f8850d865743732c03fecc857b2d943
Jean Mertz <jean@mertz.fm>
2013-06-10 15:40:15 +0200

    Allow for SSL connections using PIVOTAL_SSL env variable

--- config/initializers/pivotal_tracker.rb
+++ config/initializers/pivotal_tracker.rb
@@ -3,3 +3,4 @@ require 'pivotal-tracker'
 token = ENV['CI'] ? 'vhTkprGx7CtvotWhwy9NzoWAmpzQisjA' : ENV['PIVOTAL_TOKEN']
 raise 'No Environment variable PIVOTAL_TOKEN found' unless token
 PivotalTracker::Client.token = token
+PivotalTracker::Client.use_ssl = ENV['PIVOTAL_SSL'] == 'true' ? true : false
