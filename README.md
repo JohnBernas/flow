c6b0723499b117a380c5a69b6d0866eb0b12f694
Jean Mertz <jean@mertz.fm>
2013-07-25 13:58:55 +0200

    Update README and seed example

--- README.md
+++ README.md
@@ -1,26 +1,28 @@
 # Flow [![Build Status](https://travis-ci.org/kabisaict/flow.png?branch=master)](https://travis-ci.org/kabisaict/flow) [![Code Climate](https://codeclimate.com/github/kabisaict/flow.png)](https://codeclimate.com/github/kabisaict/flow) [![Coverage Status](https://coveralls.io/repos/kabisaict/flow/badge.png)](https://coveralls.io/r/kabisaict/flow)
 
-Flow is a KanBan layer on top of Pivotal Tracker.
+Flow is a KanBan layer on top of Zendesk.
 
 ## Benefits
 
-With Flow you can use Pivotal Tracker as a backend for all your KanBan needs.
-Synchronization is done through story statuses and labels. The application is
-still heavily in development, but the following features are currently
+With Flow you can get an internal Scrum layer on top of the Zendesk
+external support interface. You can easily rearrange priorities and get a
+quick overview where your colleagues are currently working on.
+Synchronization is done through Zendesk tags. The application is still
+heavily in development, but the following features are currently
 implemented:
 
 * Drag and drop KanBan interface
 * Websockets for pushing state between clients
-* Swimlanes based on story labels
-* Columns based on story state and labels
-* Story details in KanBan include story id, title, and a link to the Pivotal
-  story
+* Swimlanes based on ticket tags
+* One way synchronization (Zendesk -> Flow)
+* Story details in KanBan include Zendesk id, title, and a link to the
+  Zendesk ticket
 
 ## Deployment
 
-Due to the use of Websockets (and background workers) it is a bit more involved
-to deploy the application to a server. You *can* use Heroku, but you will need
-more than one worker to run the application.
+Due to the use of Websockets (and background workers) it is a bit more
+involved to deploy the application to a server. You *can* use Heroku, but
+you will need more than one worker to run the application.
 
 A "start from scratch" server setup-guide will be added to the Wiki in the
 future.
@@ -47,7 +49,7 @@ Please see [CONTRIBUTING.md][] for details.
 
 ## Credits
 
-Flow was originally written by Jean Mertz.
+Flow was originally written by [Jean Mertz].
 
 [![kabisa](http://kabisa.nl/assets/logo-7456ff79fa2f4a5d72514a807733182d.png)](http://www.kabisa.nl)
 
@@ -59,4 +61,5 @@ Flow is Copyright © 2013 Jean Mertz and Kabisa ICT. It is free software, and ma
 be redistributed under the terms specified in the [LICENSE.txt][] file.
 
 [CONTRIBUTING.md]: CONTRIBUTING.md
+[Jean Mertz]: https://twitter.com/JeanMertz
 [LICENSE.txt]: LICENSE.txt
--- db/seeds.example.rb
+++ db/seeds.example.rb
@@ -1,60 +1,55 @@
 # Board
 #
-# Specify the KanBan board details. You need to give your board a title, and the
-# Project ID of your Pivotal Tracker project.
+# Specify the KanBan board details. You need to give your board a title,
+# and the Zebdesk host.
 #
-board = Board.create(title: 'My Pivotal Project', data: { project_id: 123456 })
+board = Board.create(title: 'My Zendesk board', data: { zendesk_account: ENV['ZENDESK_HOST']})
 
 # Swimlanes
 #
-# You can create as many Swimlanes as you want. If you don't want any seperate
-# swimlanes, simply stick to a single siwmlane with `default` set to true.
+# You can create as many Swimlanes as you want. If you don't want any
+# seperate swimlanes, simply stick to a single siwmlane with `default` set
+# to true.
 #
-# You can link Pivotal Tracker stories to specific lanes by matching labels. If
-# you give the swimlane a comma seperated list of labels, any story with at
-# least one matching label will be linked to that swimlane. If a story matches
-# multiple swimlanes, the one with a higher `horizontal` rank (and thus a lower
-# integer) will be given precedence.
+# You can link Zendesk tickets to specific lanes by matching labels. If
+# you give the swimlane a comma seperated list of labels, any story with
+# at least one matching label will be linked to that swimlane. If a story
+# matches multiple swimlanes, the one with a higher `horizontal` rank (and
+# thus a lower integer) will be given precedence.
 #
 # [title] swimlane name shown above each swimlane on the page
 #
-# [horizontal] the horizontal position of the swimlane, 1 starting at the top
+# [horizontal] the horizontal position of the swimlane, 1 starting at the
+# top
 #
-# [data][limit] soft-limit of stories in a lane. The swimlane will be styled
-# appropriately if this number is superseded
+# [data][limit] soft-limit of stories in a lane. The swimlane will be
+# styled appropriately if this number is superseded
 #
-# [data][labels] story labels that should be linked to this swimlane
+# [data][labels] ticket labels that should be linked to this swimlane
 #
-# [data][default] if set to true, stories without any swimlane labels will be
-# assigned to this swimlane
+# [data][default] if set to true, stories without any swimlane labels will
+# be assigned to this swimlane
 #
 board.swimlanes.create(title: 'Criticals', horizontal: 1, data: { limit: 1, labels: 'critical' })
 board.swimlanes.create(title: 'Everything else', horizontal: 2, data: { default: true })
 
 # Columns
 #
-# Create as many KanBan columns as you want. Link them with your Pivotal Tracker
-# backend through states and labels.
+# Create as many KanBan columns as you want.
 #
 # [title] column name shown above the column contents
 #
 # [display] position of column, 1 being the left most column
 #
-# [data][default] if set to true, stories not matching any other columns will
-# end up here. Also, if you remove a column from your workflow, existing stories
-# in that column will be assigned to this one
+# [data][default] if set to true, new Zendesk tickets end up here. Also,
+# if you remove a column from your workflow, existing stories in that
+# column will be assigned to this one
 #
-# [data][state] story state linked to this column. Can be one of accepted,
-# rejected, delivered, finished, started, unstarted, unscheduled
+# [data][limit] soft-limit of maximum stories active in a column. The
+# column will be styled appropriately if this number is superseded
 #
-# [data][label] additional story label linked to this column. This allows you to
-# have multiple columns linked to the same story state, with different labels
-#
-# [data][limit] soft-limit of maximum stories active in a column. The column
-# will be styled appropriately if this number is superseded
-#
-board.columns.create(title: 'To Do', display: 1, data: { default: true, state: 'unstarted' })
-board.columns.create(title: 'On Hold', display: 2, data: { state: 'started', label: 's.hold' })
-board.columns.create(title: 'In Progress', display: 3, data: { limit: 3, state: 'started' })
-board.columns.create(title: 'Deployment', display: 4, data: { limit: 6, state: 'delivered' })
-board.columns.create(title: 'Done', display: 5, data: { state: 'accepted' })
+board.columns.create(title: 'To Do', display: 1, data: { default: true })
+board.columns.create(title: 'On Hold', display: 2)
+board.columns.create(title: 'In Progress', display: 3)
+board.columns.create(title: 'Deployment', display: 4)
+board.columns.create(title: 'Done', display: 5)
