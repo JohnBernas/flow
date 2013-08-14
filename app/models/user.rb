0fd773faec3623ef7fa0a50aef4530f081d6311d
Joao Bernardino <joao.bernardino@kabisa.nl>
2013-08-14 17:22:23 +0200

    Remove registration and recover password functionalities from the app (for now)

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  #
+  # We removed:
+  # :registerable, :recoverable
+  devise :database_authenticatable, :rememberable, :trackable, :validatable
 end
